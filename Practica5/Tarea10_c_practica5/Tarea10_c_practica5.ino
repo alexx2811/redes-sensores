bbcedec83649e7d9a2c691f4eca3a421ec93c429
alexx2811 <801058@unizar.es>
2024-03-21 16:23:20 +0100

    p5 completa y modificaciones p2

--- Practica5/Tarea10_c_practica5/Tarea10_c_practica5.ino
+++ Practica5/Tarea10_c_practica5/Tarea10_c_practica5.ino
@@ -11,13 +11,13 @@ const char server[] = "io.adafruit.com";
 const int port = 1883; // PUERTO MQTT
 const char user[] = "809847";
 // API Key de Adafruit
-const char key[] = "809847";
+const char key[] = "aio_ufLb98Nl4vSt1uDTlzUMd3Iiz03u";
 const char nombre_feed[] = "sensores";
 
 WiFiClient wifiClient;
 Adafruit_MQTT_Client mqttClient(&wifiClient, server, port, user, key);
 
-Adafruit_MQTT_Publish feed = Adafruit_MQTT_Publish(&mqttClient,"809847/feeds/sensores");
+Adafruit_MQTT_Publish feed = Adafruit_MQTT_Publish(&mqttClient, "809847/feeds/sensores");
 
 void setup() {
   Serial.begin(115200);
@@ -42,8 +42,11 @@ void loop() {
     feed.publish("8");
     Serial.println("dato enviado al feed");
   } else 
+    Serial.println("no conectado a MQTT");
+    mqttClient.disconnect();
+    delay(5000);
     mqttClient.connect();
-    Serial.println("conectando a MQTT");
+    
 
   delay(5000); // Espera 5 segundos antes de enviar otro dato
 }
