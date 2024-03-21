4448b82e7551f686ae508b135b0a7fe94f7ac365
alexx2811 <801058@unizar.es>
2024-03-21 16:23:33 +0100

    modificacion p2

--- Practica2/Tarea2/Tarea2.ino
+++ Practica2/Tarea2/Tarea2.ino
@@ -1,6 +1,7 @@
 #include <Universidad_de_Zaragoza-project-1_inferencing.h>
 
 
+
 #define EIDSP_QUANTIZE_FILTERBANK   0
 
 /**
@@ -109,7 +110,7 @@ void loop()
 
         print_results = 0;
     }
-    if(result.classification[0].value>0.95) {
+    if(result.classification[0].value> 0.85 ) {
       digitalWrite(LED_BUILTIN, HIGH);
       delay(100);
       digitalWrite(LED_BUILTIN, LOW);
